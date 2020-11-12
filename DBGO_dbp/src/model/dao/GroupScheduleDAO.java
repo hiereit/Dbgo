19b08f8fb8709a39f2b45d2097dd676df18ba47d
yyj424 <69260313+yyj424@users.noreply.github.com>
2020-11-13 02:09:29 +0900

    GroupDAO, GroupScheduleDAO

--- DBGO_dbp/src/model/dao/GroupScheduleDAO.java
+++ DBGO_dbp/src/model/dao/GroupScheduleDAO.java
@@ -8,11 +8,11 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import model.dto.GroupInfo;
+import model.dto.GroupSchedule;
 
-public class GroupDAO {
-	
-	public GroupDAO() {
+public class GroupScheduleDAO {
+
+	public GroupScheduleDAO() {
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");	
 		} catch (ClassNotFoundException ex) {
@@ -36,93 +36,56 @@ public class GroupDAO {
 		return conn;
 	}
 	
-	public List<GroupInfo> getGroupList() {
+	public List<GroupSchedule> getGroupScheduleList(String g_id) {
 		Connection conn = null;
 		PreparedStatement pStmt = null;		// PreparedStatment 참조 변수 생성
 		ResultSet rs = null;		
 		String query = "SELECT * " 
-					 + "FROM GROUPINFO";
-		
-		try {
-			conn = getConnection();	// DBMS와의 연결 획득 
-			pStmt = conn.prepareStatement(query);	// Connection에서 PreparedStatement 객체 생성
-			rs = pStmt.executeQuery();	
-			
-			List<GroupInfo> groupList = new ArrayList<GroupInfo>();
-			while (rs.next()) {		// 커서를 통해 한 행씩 fetch
-				GroupInfo g = new GroupInfo(rs.getString("g_id"), rs.getString("g_name"));
-				groupList.add(g);
-			}
-			return groupList;
-		} catch (SQLException ex) {
-			ex.printStackTrace();
-		} finally {		// 자원 반납
-			if (rs != null) 
-				try { 
-					rs.close(); 
-				} catch (SQLException ex) { ex.printStackTrace(); }
-			if (pStmt != null) 
-				try { 
-					pStmt.close(); 
-				} catch (SQLException ex) { ex.printStackTrace(); }
-			if (conn != null) 
-				try { 
-					conn.close(); 
-				} catch (SQLException ex) { ex.printStackTrace(); }
-		}
-		return null;
-	}
-	
-	public List<GroupInfo> getMyGroupList(String u_id) {
-		Connection conn = null;
-		PreparedStatement pStmt = null;		// PreparedStatment 참조 변수 생성
-		ResultSet rs = null;		
-		String query = "SELECT g.g_id, g.g_name " 
-					 + "FROM ADMISSION a JOIN GROUP g USING (g_id) "
-					 + "WHERE a.u_id = ?";
-		
-		try {
-			conn = getConnection();	// DBMS와의 연결 획득 
-			pStmt = conn.prepareStatement(query);	// Connection에서 PreparedStatement 객체 생성
-			pStmt.setString(1, u_id);
-			rs = pStmt.executeQuery();	
-			
-			List<GroupInfo> groupList = new ArrayList<GroupInfo>();
-			while (rs.next()) {		// 커서를 통해 한 행씩 fetch
-				GroupInfo g = new GroupInfo(rs.getString("g_id"), rs.getString("g_name"));
-				groupList.add(g);
-			}
-			return groupList;
-		} catch (SQLException ex) {
-			ex.printStackTrace();
-		} finally {		// 자원 반납
-			if (rs != null) 
-				try { 
-					rs.close(); 
-				} catch (SQLException ex) { ex.printStackTrace(); }
-			if (pStmt != null) 
-				try { 
-					pStmt.close(); 
-				} catch (SQLException ex) { ex.printStackTrace(); }
-			if (conn != null) 
-				try { 
-					conn.close(); 
-				} catch (SQLException ex) { ex.printStackTrace(); }
-		}
-		return null;
-	}
-	
-	public void removeGroup(String g_id) {
-		Connection conn = null;
-		PreparedStatement pStmt = null;		// PreparedStatment 참조 변수 생성
-		ResultSet rs = null;
-		String query = "DELETE FROM group "
+					 + "FROM GROUP_SCHEDULE "
 					 + "WHERE g_id = ?";
 		
 		try {
 			conn = getConnection();	// DBMS와의 연결 획득 
 			pStmt = conn.prepareStatement(query);	// Connection에서 PreparedStatement 객체 생성
-			pStmt.setString(1, g_id);
+			rs = pStmt.executeQuery();	
+			
+			List<GroupSchedule> groupScheduleList = new ArrayList<GroupSchedule>();
+			while (rs.next()) {		// 커서를 통해 한 행씩 fetch
+				GroupSchedule gsch = new GroupSchedule(rs.getString("homework"), rs.getString("g_id"),
+						rs.getString("groupsch_id"), rs.getDate("groupsch_date"), rs.getString("memo"), rs.getString("title"));
+				groupScheduleList.add(gsch);
+			}
+			return groupScheduleList;
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		} finally {		// 자원 반납
+			if (rs != null) 
+				try { 
+					rs.close(); 
+				} catch (SQLException ex) { ex.printStackTrace(); }
+			if (pStmt != null) 
+				try { 
+					pStmt.close(); 
+				} catch (SQLException ex) { ex.printStackTrace(); }
+			if (conn != null) 
+				try { 
+					conn.close(); 
+				} catch (SQLException ex) { ex.printStackTrace(); }
+		}
+		return null;
+	}
+	
+	public void removeGroupSchedule(String groupsch_id) {
+		Connection conn = null;
+		PreparedStatement pStmt = null;		// PreparedStatment 참조 변수 생성
+		ResultSet rs = null;
+		String query = "DELETE FROM group_schedule "
+					 + "WHERE groupsch_id = ?";
+		
+		try {
+			conn = getConnection();	// DBMS와의 연결 획득 
+			pStmt = conn.prepareStatement(query);	// Connection에서 PreparedStatement 객체 생성
+			pStmt.setString(1, groupsch_id);
 			int result = pStmt.executeUpdate();
 			
 			if (result != 1) {
@@ -155,19 +118,19 @@ public class GroupDAO {
 		}
 	}
 	
-	public void updateGroup(String g_id, String g_name) {
+	public void updateGroup(String groupsch_id, String title) {
 		Connection conn = null;
 		PreparedStatement pStmt = null;		// PreparedStatment 참조 변수 생성
 		ResultSet rs = null;		
-		String query = "UPDATE group " 
-					 + "SET g_name = ? "
-					 + "WHERE g_id = ?";
+		String query = "UPDATE group_schedule " 
+					 + "SET title = ? "
+					 + "WHERE groupsch_id = ?";
 		
 		try {
 			conn = getConnection();	// DBMS와의 연결 획득 
 			pStmt = conn.prepareStatement(query);	// Connection에서 PreparedStatement 객체 생성
-			pStmt.setString(1, g_name);
-			pStmt.setString(2, g_id);
+			pStmt.setString(1, title);
+			pStmt.setString(2, groupsch_id);
 			int result1 = pStmt.executeUpdate();
 			
 			if (result1 != 1) {
@@ -200,25 +163,21 @@ public class GroupDAO {
 		}
 	}
 	
-	public void createGroup(String u_id, String g_name) {
+	public void createGroup(String g_id, String title) {
 		Connection conn = null;
 		PreparedStatement pStmt = null;		// PreparedStatment 참조 변수 생성
 		ResultSet rs = null;		
-		String query1 = "INSERT INTO ADMISSION (u_id) VALUES (?)";
-		String query2 = "INSERT INTO GROUP (g_name) VALUES (?)";
+		String query = "INSERT INTO GROUP_SCHEDULE (g_id, title) VALUES (?, ?)";
 		
 		try {
 			conn = getConnection();	// DBMS와의 연결 획득 
-			pStmt = conn.prepareStatement(query1);	// Connection에서 PreparedStatement 객체 생성
-			pStmt.setString(1, u_id);
+			pStmt = conn.prepareStatement(query);	// Connection에서 PreparedStatement 객체 생성
+			pStmt.setString(1, g_id);
+			pStmt.setString(2, title);
 			int result1 = pStmt.executeUpdate();
-			pStmt.close();						// pStmt가 가리키는 객체 close!
-
-			pStmt = conn.prepareStatement(query2);	
-			pStmt.setString(1, g_name);
-			int result2 = pStmt.executeUpdate();
+			pStmt.close();						
 			
-			if (result1 != 1 || result2 != 1) {
+			if (result1 != 1) {
 				conn.rollback();
 			}
 			else {
