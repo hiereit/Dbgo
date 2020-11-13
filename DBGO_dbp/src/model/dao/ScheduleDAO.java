02b0185b8995abf4ec669f55f2e86651d82d47d2
hiereit <68723848+hiereit@users.noreply.github.com>
2020-11-13 14:36:46 +0900

    commot_sera / Schedule , SchedeuleDAO

---
+++ DBGO_dbp/src/model/dao/ScheduleDAO.java
@@ -0,0 +1,190 @@
+package model.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Date;
+
+import model.dto.Schedule;
+
+public class ScheduleDAO {
+	Connection conn = null;
+	PreparedStatement pStmt = null;
+	ResultSet rs = null;	
+	
+	public ScheduleDAO() { // 생성자 
+        // JDBC driver 로딩 및 등록 ... 
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");	
+		} catch (ClassNotFoundException ex) {
+			ex.printStackTrace();
+		}	
+
+    } 
+	private Connection getConnection() {		
+		String url = // "jdbc:oracle:thin:@localhost:1521:xe";
+				"jdbc:oracle:thin:@202.20.119.117:1521:orcl";	
+		String user = "dbprog0208";
+		String passwd = "qkrprh5";
+
+	    // DBMS와의 연결 획득 ...
+		try {
+			conn = DriverManager.getConnection(url, user, passwd);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}	 
+		return conn;
+    }
+	public void insertSchedule(int sch_id, String sch_date,
+			String memo, String title, String category, int u_id, int acv) {
+		String query = "INSERT INTO SCHEDULE "
+					 + "VALUES(?, ?, ?, ?, ?, ?, ?)";
+		try {
+			conn = getConnection();
+			pStmt = conn.prepareStatement(query);
+			pStmt.setInt(1, sch_id);
+			pStmt.setString(2, sch_date);
+			pStmt.setString(3, memo);
+			pStmt.setString(4, title);
+			pStmt.setString(5, category);
+			pStmt.setInt(6, u_id);
+			pStmt.setInt(7, acv);			
+			
+			pStmt.executeUpdate();
+			//System.out.println("사원 번호\t사원이름\t직무\t월급\t수당");
+			
+		}catch(SQLException ex) {
+			ex.printStackTrace();
+		} finally {	
+			close();
+		}
+	}
+	public void updateSchedule(String memo, String title, String category) {
+		String query = "UPDATE SCHEDULE SET "
+					 + "memo = ? "
+					 + ", category = ? "
+					 + "WHERE title = ?";
+		try {
+			conn = getConnection();
+			pStmt = conn.prepareStatement(query);
+			pStmt.setString(1, memo);
+			pStmt.setString(2, category);	
+			pStmt.setString(3, title);
+			pStmt.executeUpdate();
+			
+		}catch(SQLException ex) {
+			ex.printStackTrace();
+		} finally {	
+			close();
+		}
+	}
+	public void deleteSchedule(String u_id) {
+		String query = "DELETE FROM SCHEDULE "
+					 + "WHERE title = ?";
+		try {
+			conn = getConnection();
+			pStmt = conn.prepareStatement(query);
+			pStmt.setString(1, u_id);
+			pStmt.executeUpdate();
+			
+		}catch(SQLException ex) {
+			ex.printStackTrace();
+		} finally {	
+			close();
+		}
+	}
+	public Schedule findScheduleInfo(String schTitle) {
+		String query = "SELECT * "
+					+ "FROM schedule "
+					+ "WHERE title = ? ";
+		
+		Schedule sch = null;
+		 
+		try {
+			conn = getConnection();
+			pStmt = conn.prepareStatement(query);
+			pStmt.setString(1, schTitle);
+			
+			rs = pStmt.executeQuery();
+			//System.out.println("사원 번호\t사원이름\t직무\t월급\t수당");
+			if(rs.next()) {
+				
+				int sch_id = rs.getInt("sch_id");
+				Date sch_date = rs.getDate("sch_date");
+				String memo = rs.getString("memo");
+				String title = rs.getString("title");
+				String category = rs.getString("category");
+				int u_id = rs.getInt("u_id");
+				int achievement = rs.getInt("achievement");
+				
+		
+				sch = new Schedule(sch_id, sch_date, memo, title, category, u_id, achievement);
+				
+				return sch;
+			}
+		}catch(SQLException ex) {
+			ex.printStackTrace();
+		} finally {	
+			close();
+		}
+		return null;
+	
+	}
+	 public List<Schedule> findSchedulesInDept(String userId) {
+
+	        // printEmployeesInDept()를 변형 ...
+	    	String query =  "SELECT * "
+			  		+ "FROM schedule "
+			  		+ "WHERE userId = ?";
+	    	try {
+		    	conn = getConnection();
+		    	pStmt = conn.prepareStatement(query);
+		    	pStmt.setString(1, userId);
+				rs = pStmt.executeQuery();    	
+		        
+		    	// ArrayList<Employee> 객체를 생성하고, 검색된 각 사원 정보에 대해 Employee 객체를 생성 및 저장하고, 그 객체를 ArrayList에 추가함
+		    	List<Schedule> schList = new ArrayList<Schedule>();
+		    	
+		    	Schedule sch = new Schedule();
+		    	
+		    	while(rs.next()) {	    
+		    		sch.setSchId(rs.getString("sch_id"));
+		    		sch.setSchDate(rs.getDate("sch_date"));
+					sch.setMemo(rs.getString("memo"));
+					sch.setTitle(rs.getString("title"));
+					sch.setCategory(rs.getString("category"));
+					sch.setU_id(rs.getInt("u_id"));
+					sch.setAchievement(rs.getInt("achievement"));
+			
+					schList.add(sch);				
+				}		
+		    	return schList;
+	    	} catch(SQLException ex) {
+	    		ex.printStackTrace();
+	    	} finally {
+	    		close();
+	    	}
+			return null;
+	    }
+	
+	
+	
+	public void close() { //자원반납
+    	if (rs != null) 
+			try { 
+				rs.close(); 
+			} catch (SQLException ex) { ex.printStackTrace(); }
+		if (pStmt != null) 
+			try { 
+				pStmt.close(); 
+			} catch (SQLException ex) { ex.printStackTrace(); }
+		if (conn != null) 
+			try { 
+				conn.close(); 
+			} catch (SQLException ex) { ex.printStackTrace(); }
+    }
+}
--- DBGO_dbp/src/model/dto/Schedule.java
+++ DBGO_dbp/src/model/dto/Schedule.java
@@ -2,20 +2,20 @@ package model.dto;
 import java.util.Date;
 
 public class Schedule {
-	private String schId, memo, title, category, u_id;
-	private int achievement;
-	private Date shcDate;
+	private String memo, title, category;
+	private int achievement, u_id, schId;
+	private Date schDate;
 	
 	//Constructor (default/ ...)
 	public Schedule() { //default 持失切
 		super();
 	}
 	
-	public Schedule(String schId, Date shcDate, String memo, 
-			String title, String category, String u_id,	int achievement) {
+	public Schedule(int sch_id, Date schDate, String memo, 
+			String title, String category, int u_id,	int achievement) {
 		super();
-		this.schId = schId;
-		this.shcDate = shcDate;
+		this.schId = sch_id;
+		this.schDate = schDate;
 		this.memo = memo;
 		this.title = title;
 		this.category = category;
@@ -24,24 +24,29 @@ public class Schedule {
 	}	
 	
 	//setter & getter 識情
-	public String getSchId() {		return schId;	}
-	public void setSchId(String schId) {		this.schId = schId;	}
-	public Date getShcDate() {		return shcDate;	}
-	public void setShcDate(Date shcDate) {		this.shcDate = shcDate;	}
+	public int getSchId() {		return schId;	}
+	public void setSchId(String string) {		this.schId = schId;	}
+	public Date getSchDate() {		return schDate;	}
+	public void setSchDate(Date schDate) {		this.schDate = schDate;	}
 	public String getMemo() {		return memo;	}
 	public void setMemo(String memo) {		this.memo = memo;	}
 	public String getTitle() {		return title;	}
 	public void setTitle(String title) {		this.title = title;	}
 	public String getCategory() {		return category;	}
 	public void setCategory(String category) {		this.category = category;	}
-	public String getU_id() {		return u_id;	}
-	public void setU_id(String u_id) {		this.u_id = u_id;	}
+	public int getU_id() {		return u_id;	}
+	public void setU_id(int u_id) {		this.u_id = u_id;	}
 	public int getAchievement() {		return achievement;	}
 	public void setAchievement(int achievement) {		this.achievement = achievement;	}
 	
 	public String toString() {
 		return "";
-	} 
-	
+	}
+
+	public void setSchDate(java.sql.Date date) {
+		// TODO Auto-generated method stub
+		this.schDate = schDate;
+	}
+
 
 }
